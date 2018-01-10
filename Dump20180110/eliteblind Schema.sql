f82e3bdb608f144e52ba6d3c7905071d0512c9c6
Suresh Kanniyappan <skanniyappan@aptean.com>
2018-01-10 10:51:31 +0530

    DB Backup scripts

---
+++ Dump20180110/eliteblind Schema.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE `eliteblinds` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
---
+++ Dump20180110/eliteblinds_control.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE  IF NOT EXISTS `eliteblinds` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `eliteblinds`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: eliteblinds
+-- ------------------------------------------------------
+-- Server version	5.7.20-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `control`
+--
+
+DROP TABLE IF EXISTS `control`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `control` (
+  `ControlID` int(11) NOT NULL AUTO_INCREMENT,
+  `ControlDesc` varchar(1000) DEFAULT NULL,
+  `For` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`ControlID`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `control`
+--
+
+LOCK TABLES `control` WRITE;
+/*!40000 ALTER TABLE `control` DISABLE KEYS */;
+INSERT INTO `control` VALUES (1,'MOTOR','OVertical'),(2,'MANUAL','OVertical'),(3,'MOTOR','OVenetian'),(4,'MANUAL','OVenetian'),(5,'MANUAL','ORoller'),(6,'MANUAL WITH HEADER','ORoller'),(7,'MANUAL WITH FASCIA BOX','ORoller'),(8,'MOTOR','ORoller'),(9,'MANUAL','OZebra'),(10,'MOTOR','OZebra');
+/*!40000 ALTER TABLE `control` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-01-10 10:41:40
